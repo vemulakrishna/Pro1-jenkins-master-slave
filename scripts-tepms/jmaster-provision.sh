04027bf916c087b30491155d97e3fd2efa196bb9
krishna vemula <vkr5781@gmail.com>
2019-04-10 20:10:02 +0530

    Update jmaster-provision.sh

--- scripts-tepms/jmaster-provision.sh
+++ scripts-tepms/jmaster-provision.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 installJenkins() {
+        sudo add-apt-repository ppa:webupd8team/java
+        sudo apt install oracle-java8-installer
 	wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 	sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-	apt-get update -y && apt-get install jenkins=2.121.1 -y	# 2.73.3 -y 
+	apt-get update -y && apt-get install jenkins 
 	sudo systemctl start jenkins
 }
 
